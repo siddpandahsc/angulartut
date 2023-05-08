49272015b8ae5c1fceded5b6d74106d4d62ca980
siddpandahsc <siddheshwar.panda@hsc.com>
2023-05-08 14:49:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // template:'<h1>Hellow </h1>',
+  // template: `<h1>Hellow</h1>
+  //           <h3>Hellowwwww</h3>`,
+  styleUrls: ['./app.component.scss'],
+  // styles:[`h1 {color:red;}`]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
