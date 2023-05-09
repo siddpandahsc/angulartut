daba9cbfd18658cd5345819d9a4eb8b314ac202d
siddpandahsc <siddheshwar.panda@hsc.com>
2023-05-09 11:20:53 +0530

    ngfor

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Room } from './rooms';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -12,11 +12,44 @@ export class RoomsComponent {
 
   hideRooms = false;
 
-  rooms : Room = {
-    availableRooms : 0,
-    bookedRooms : 5,
-    totalRooms:20
+  // rooms : Room = {
+
+  // }
+  rooms: Room = {
+    availableRooms: 3,
+    bookedRooms: 5,
+    totalRooms: 20
   }
+
+  roomList: RoomList[] = [
+    {
+      roomNumber:1,
+      roomType: "Deluxe Room",
+      amenities: "AC, Double Bed",
+      price: 500,
+      photos: "https://images.unsplash.com/photo-1578683010236-d716f9a3f461?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime:new Date('13-Nov-2021'),
+    },
+    {
+      roomNumber:2,
+      roomType: "Premium Room",
+      amenities: "AC, Double Bed, Bathroom",
+      price: 1500,
+      photos: "https://images.unsplash.com/photo-1578683010236-d716f9a3f461?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      checkinTime: new Date('13-Nov-2021'),
+      checkoutTime:new Date('18-Nov-2021'),
+    },
+    {
+      roomNumber:3,
+      roomType: "Private Suite Room",
+      amenities: "AC, Double Bed, Bathroom, Toilet",
+      price: 1500,
+      photos: "https://images.unsplash.com/photo-1578683010236-d716f9a3f461?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      checkinTime: new Date('13-Nov-2021'),
+      checkoutTime:new Date('18-Nov-2021'),
+    },
+  ]
 
   toggle() {
     this.hideRooms = !this.hideRooms;
